7bdee740855780268c2a04a252e32d9eb5b544c4
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-06 21:47:06 -0300

    bot update the group description when receive the /update command

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
 const URI = `/webhook/${BOT_TOKEN}`;
 const WEBHOOK_URL = SERVER_URL + URI;
 
+const updateRegex = /\/update$|\/update@quantosdiaspronatalbot$/;
+
 const init = async () => {
   const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
   console.log(res.data);
@@ -18,6 +20,24 @@ app.use(express.json());
 app.post(URI, async (req, res) => {
   const data = req.body;
   console.log(data);
+
+  if (data.message === undefined) {
+    return res.send();
+  }
+
+  if (updateRegex.test(data.message.text)) {
+    if (data.message.chat.type === "supergroup") {
+      try {
+        await axios.post(`${TELEGRAM_API}/setChatDescription`, {
+          chat_id: data.message.chat.id,
+          description: "Descrição atualizada",
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
   return res.send();
 });
 
